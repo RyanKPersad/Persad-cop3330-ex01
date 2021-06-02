1201afbebf253abb0c203a3c81caaea0c865861e
ryanpersad <Rpersad0305@gmail.com>
2021-06-02 17:24:43 -0400

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,6 +12,9 @@ public class App
         Scanner myObj = new Scanner(System.in);
         System.out.println( "What is your name?" );
         String name =  myObj.nextLine();
-        System.out.println("Hello, " + name + " nice to meet you!");
+        //Output
+        String output = ("Hello, " + name + " nice to meet you!");
+        System.out.println(output);
+
     }
 }
